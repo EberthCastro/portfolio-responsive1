6d262d0819d0387f8d8969b827c7278d5f37fb8a
Eberth <itec_19@hotmail.com>
2022-10-12 19:43:00 +0200

    styling navbar in sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import Navbar from './components/Navbar'
+import NBar from './components/NBar'
 
 export default function App() {
     return (
         <div>
-            <Navbar/>
+            <NBar/>
         </div>
     )
 }
