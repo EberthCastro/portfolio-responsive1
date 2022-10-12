ac768c90299ade0933698d6b4f6248475b845590
Eberth <itec_19@hotmail.com>
2022-10-12 17:58:20 +0200

    defining files structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 export default function App() {
     return (
         <div>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload. By Isaac Castro Dev
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-
+            <Navbar/>
         </div>
     )
 }
