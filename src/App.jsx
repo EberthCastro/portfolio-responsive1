0216fee6ac7e64ee22f443be753434844759dd15
Eberth <itec_19@hotmail.com>
2022-10-14 14:54:56 +0200

    project and project card done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import NBar from './components/NBar'
 import Banner from './components/Banner'
 import Skills from './components/Skills'
+import Projects from './components/Projects'
 
 export default function App() {
     return (
@@ -11,6 +12,7 @@ export default function App() {
             <NBar/>
             <Banner/>
             <Skills/>
+            <Projects/>
         </div>
     )
 }
